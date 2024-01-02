a0168927bb54737d9aa143a295b7195471f8d42c
Nicholas Berlette <nick@berlette.com>
2024-01-02 08:23:25 -0800

    refactor: export Options from project root

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
 import { check, checkSync, format, formatSync, Formatter } from "./src/fmt.ts";
+import { Options } from "./src/options.ts";
 
-export { check, checkSync, format, formatSync, Formatter };
+export { check, checkSync, format, formatSync, Formatter, Options };
 
-export default { format, formatSync, check, checkSync, Formatter };
+export default { format, formatSync, check, checkSync, Formatter, Options };
 
-export { default as defaultOptions, Options } from "./src/options.ts";
+export { default as defaultOptions } from "./src/options.ts";
